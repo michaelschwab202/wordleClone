af1d6353a53a654a0eb49b7260d28d643cd67f37
Michael Schwab <mfschwa@clemson.edu>
2025-10-22 20:43:10 -0400

    Improved Navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,8 +6,8 @@ export default function Header()
         <div className="header">
             <h1 id="siteNameDisplay">WORDLE CLONE</h1>
             <div className="navbar">
-                <a href="https://github.com/michaelschwab202">Michael Schwab's GitHub</a>
-                <a>Source Code for this Page</a>
+                <a href="https://github.com/michaelschwab202">MICHAEL SCHWAB'S GITHUB</a>
+                <a>SOURCE CODE FOR WORDLE CLONE</a>
             </div>
             
         </div>
